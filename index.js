3cb46c55266da7125090e027dd0e4f0ccfa0f5d8
sannypro <sannymr79@gmail.com>
2022-05-22 16:54:25 +0600

    single product load api done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 // 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrbxe.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,6 +19,14 @@ async function run() {
             const query = {}
             const result = await carPartsCollection.find(query).toArray();
             res.send(result)
+        })
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+
+            const result = await carPartsCollection.findOne(query);
+            res.send(result)
+            console.log(result);
         })
     }
     finally {
